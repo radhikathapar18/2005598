d289747612ac84b2758d7eaf04b2347dc643db6c
radhika <radhikathapar1@gmail.com>
2023-07-21 15:26:11 +0530

    Your commit message here

--- q1.py
+++ q1.py
@@ -1,29 +1,41 @@
-import requests
-from flask import Flask, request, jsonify
-from gevent import monkey
-import gevent
-monkey.patch_all()
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const axios = require("axios");
 
-app = Flask(__name__)
+const requestTimeout = 500; // Set the desired response time in milliseconds
 
-def get_numbers_from_url(url):
-    try:
-        response = requests.get(url, timeout=0.5)
-        if response.status_code == 200:
-            data = response.json()
-            if 'numbers' in data and isinstance(data['numbers'], list):
-                return data['numbers']
-    except (requests.exceptions.Timeout, requests.exceptions.RequestException):
-        pass
-    return []
+dotenv.config();
 
-@app.route('/numbers')
-def get_merged_numbers():
-    urls = request.args.getlist('url')
-    jobs = [gevent.spawn(get_numbers_from_url, url) for url in urls]
-    gevent.joinall(jobs, timeout=0.5)
-    merged_numbers = sorted(list(set(num for job in jobs for num in job.value)))
-    return jsonify(numbers=merged_numbers)
+const app = express();
+const PORT = process.env.PORT || 8008;
 
-if __name__ == '__main__':
-    app.run(host='localhost', port=8008)
+app.use(cors());
+
+app.get("/numbers", async (req, res) => {
+  const urls = req.query.url;
+  // console.log(urls);
+
+  if (!urls) return res.status(400).json({ error: "Invalid URL" });
+
+  try {
+    const promises = urls.map(async (url) => {
+      const response = await axios.get(url, { timeout: requestTimeout });
+      return response.data.numbers;
+    });
+
+    const numbersArray = await Promise.all(promises);
+    // console.log(numbersArray);
+    // debugger
+    const flattenedArray = [...new Set(numbersArray.flat())];
+    flattenedArray.sort((a, b) => a - b);
+
+    res.json({ numbers: flattenedArray });
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
